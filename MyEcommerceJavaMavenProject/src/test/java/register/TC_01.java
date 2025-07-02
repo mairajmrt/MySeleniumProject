735d5572bc4bb68e764f21442912a70175441e2c
mali204 <mali204@DUC-LAn20q3gaZ3.dedalus.lan>
2025-07-02 19:13:09 +0530

    some code is commited

--- MyEcommerceJavaMavenProject/src/test/java/register/TC_01.java
+++ MyEcommerceJavaMavenProject/src/test/java/register/TC_01.java
@@ -1,10 +1,16 @@
 package register;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TC_01 {
-
 	public static void main(String[] args) {
-	System.out.println("Hello Java By Mairaj Ali");
-
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo/");	
 	}
 
 }
