8a24cb5ddf25041f880574e6aed8bdd10af69c6d
mali204 <mali204@DUC-LAn20q3gaZ3.dedalus.lan>
2025-07-03 17:51:48 +0530

    TC_01 case is tested and committed by mairaj

--- MyEcommerceJavaMavenProject/src/test/java/register/TC_01.java
+++ MyEcommerceJavaMavenProject/src/test/java/register/TC_01.java
@@ -1,16 +1,38 @@
 package register;
-
 import java.time.Duration;
-
+import java.util.Date;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class TC_01 {
-	public static void main(String[] args) {
+	@Test
+	public void getRegister() {
+		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		driver.manage().window().maximize();
-		driver.get("https://tutorialsninja.com/demo/");	
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.findElement(By.xpath("//span[normalize-space()='My Account']")).click();
+		driver.findElement(By.xpath("//a[normalize-space()='Register']")).click();
+		driver.findElement(By.id("input-firstname")).sendKeys("mairaj");
+		driver.findElement(By.id("input-lastname")).sendKeys("ali");
+		driver.findElement(By.id("input-email")).sendKeys(getEmailAddress());
+		driver.findElement(By.id("input-telephone")).sendKeys("9634068065");
+		driver.findElement(By.id("input-password")).sendKeys("Maa@1987");
+		driver.findElement(By.id("input-confirm")).sendKeys("Maa@1987");
+		driver.findElement(By.xpath("//label[normalize-space()='Yes']//input[@name='newsletter']")).click();
+		driver.findElement(By.xpath("//input[@name='agree']")).click();
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		String expectedmsg="Your Account Has Been Created!";
+		String actmsg=driver.findElement(By.xpath("//h1[normalize-space()='Your Account Has Been Created!']")).getText();
+		Assert.assertEquals(actmsg, expectedmsg);	
 	}
-
+	
+	public String getEmailAddress() {
+		String emailaddress=new Date().toString().replaceAll("\\ ", "").replaceAll("\\:","")+"@gmail.com";
+		return emailaddress;
+	}
 }
