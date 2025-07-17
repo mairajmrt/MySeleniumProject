7d9a372fe2655f3652bf8d634fdb1d6e3b65d045
mali204 <mali204@DUC-LAn20q3gaZ3.dedalus.lan>
2025-07-17 19:24:16 +0530

    TC4 is committed by mairaj ali

--- MyEcommerceJavaMavenProject/src/test/java/RegistrationPage/TC_04.java
+++ MyEcommerceJavaMavenProject/src/test/java/RegistrationPage/TC_04.java
@@ -2,39 +2,39 @@ package RegistrationPage;
 
 import java.time.Duration;
 import java.util.Date;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TC_04 {
 
 	@Test
-	public static void getAllFieldsRegistration() {
-		WebDriver driver=new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	public void registerNonSubscriber() {
+
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		driver.manage().window().maximize();
 		driver.get("https://tutorialsninja.com/demo/");
 		driver.findElement(By.linkText("My Account")).click();
 		driver.findElement(By.linkText("Register")).click();
 		driver.findElement(By.id("input-firstname")).sendKeys("mairaj");
 		driver.findElement(By.id("input-lastname")).sendKeys("ali");
-		String emailAddress=getEMailAddress()+"@gmail.com";
-		driver.findElement(By.id("input-email")).sendKeys(emailAddress);
+		String emailid = new Date().toString().replaceAll("\\s", "").replaceAll("\\:", "") + "@gmail.com";
+		driver.findElement(By.id("input-email")).sendKeys(emailid);
 		driver.findElement(By.id("input-telephone")).sendKeys("9634068065");
-		driver.findElement(By.id("input-password")).sendKeys("adden");
-		driver.findElement(By.id("input-confirm")).sendKeys("adden");
+		driver.findElement(By.id("input-password")).sendKeys("Adden@1982");
+		driver.findElement(By.id("input-confirm")).sendKeys("Adden@1982");
 		driver.findElement(By.xpath("//label[normalize-space()='Yes']//input[@name='newsletter']")).click();
 		driver.findElement(By.xpath("//input[@name='agree']")).click();
 		driver.findElement(By.xpath("//input[@value='Continue']")).click();
-		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		driver.findElement(By.xpath("//a[@class='btn btn-primary']")).click();
 		driver.findElement(By.xpath("//a[normalize-space()='Subscribe / unsubscribe to newsletter']")).click();
-		driver.findElement(By.xpath("//input[@value='Continue']")).click();
-	}
-	
-	public static String getEMailAddress() {
-		Date date=new Date();
-		return date.toString().replaceAll("\\s","").replaceAll("\\:","");
+		Assert.assertTrue(
+				driver.findElement(By.xpath("//h1[normalize-space()='Newsletter Subscription']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//input[@value='1']")).isSelected());
 	}
 
 }
