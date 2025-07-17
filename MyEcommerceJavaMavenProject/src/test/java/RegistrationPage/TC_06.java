f1681f5e156b95b24947f31c308d97a003c0ede9
mali204 <mali204@DUC-LAn20q3gaZ3.dedalus.lan>
2025-07-17 19:01:24 +0530

    TC 6 is committed by mairaj ali

---
+++ MyEcommerceJavaMavenProject/src/test/java/RegistrationPage/TC_06.java
@@ -0,0 +1,40 @@
+package RegistrationPage;
+
+import java.time.Duration;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_06 {
+
+	@Test
+	public void registerNonSubscriber() {
+
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.findElement(By.linkText("My Account")).click();
+		driver.findElement(By.linkText("Register")).click();
+		driver.findElement(By.id("input-firstname")).sendKeys("mairaj");
+		driver.findElement(By.id("input-lastname")).sendKeys("ali");
+		String emailid = new Date().toString().replaceAll("\\s", "").replaceAll("\\:", "") + "@gmail.com";
+		driver.findElement(By.id("input-email")).sendKeys(emailid);
+		driver.findElement(By.id("input-telephone")).sendKeys("9634068065");
+		driver.findElement(By.id("input-password")).sendKeys("Adden@1982");
+		driver.findElement(By.id("input-confirm")).sendKeys("Adden@1982");
+		driver.findElement(By.xpath("//input[@value='0']")).click();
+		driver.findElement(By.xpath("//input[@name='agree']")).click();
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		driver.findElement(By.xpath("//a[@class='btn btn-primary']")).click();
+		driver.findElement(By.xpath("//a[normalize-space()='Subscribe / unsubscribe to newsletter']")).click();
+		Assert.assertTrue(
+				driver.findElement(By.xpath("//h1[normalize-space()='Newsletter Subscription']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//input[@value='0']")).isSelected());
+	}
+
+}
