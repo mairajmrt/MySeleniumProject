468d5907cfe043dadb8c81054e49a06dc4f2c260
mali204 <mali204@DUC-LAn20q3gaZ3.dedalus.lan>
2025-09-05 14:44:08 +0530

    ggggggggg

--- MyEcommerceJavaMavenProject/src/test/java/RegistrationPage/TC_29.java
+++ MyEcommerceJavaMavenProject/src/test/java/RegistrationPage/TC_29.java
@@ -1,12 +1,20 @@
 package RegistrationPage;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import org.openqa.selenium.By;
-import org.testng.Assert;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.io.FileHandler;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import Base.Base;
-import GlobalUtility.CommonUtils;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.comparison.ImageDiff;
+import ru.yandex.qatools.ashot.comparison.ImageDiffer;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 
 public class TC_29 extends Base {
 
@@ -26,25 +34,29 @@ public class TC_29 extends Base {
 
 	@Test
 
-	void verifyRegisterPageDiffEnv() throws InterruptedException {
+	void comapreImage() throws IOException {
 
 		driver.get("https://tutorialsninja.com/demo/");
-		driver.findElement(By.linkText("My Account")).click();
-		driver.findElement(By.linkText("Register")).click();
-		driver.findElement(By.id("input-firstname")).sendKeys("Mairaj");
-		driver.findElement(By.id("input-lastname")).sendKeys("Ali");
-		driver.findElement(By.id("input-email")).sendKeys(CommonUtils.getNewEmailAddress());
-		driver.findElement(By.id("input-telephone")).sendKeys("1266666");
-		driver.findElement(By.id("input-password")).sendKeys("1212");
-		driver.findElement(By.id("input-confirm")).sendKeys("1212");
-		driver.findElement(By.xpath("//label[normalize-space()='Yes']//input[@name='newsletter']")).click();
-		driver.findElement(By.xpath("//input[@name='agree']")).click();
-		driver.findElement(By.xpath("//input[@value='Continue']")).click();
-		Thread.sleep(4000);
-		String expectedmsg = "Your Account Has Been Created!";
-		String actmsg = driver.findElement(By.xpath("//h1[normalize-space()='Your Account Has Been Created!']"))
-				.getText();
-		Assert.assertEquals(actmsg, expectedmsg);
+		WebElement logo = driver.findElement(By.xpath("//a[normalize-space()='Qafox.com']"));
+		File rscFile=logo.getScreenshotAs(OutputType.FILE);
+		File dscFile=new File("C:\\Users\\mali204\\git\\MySeleniumProject\\MyEcommerceJavaMavenProject\\image\\abc.png");
+		FileHandler.copy(rscFile, dscFile);
+		
+		// Take screenshot with AShot
+		BufferedImage actualImage = new AShot().shootingStrategy(ShootingStrategies.simple()).takeScreenshot(driver, logo).getImage();
+		
+		// Load expected image
+		BufferedImage expectedImage = ImageIO.read(new File("C:\\Users\\mali204\\git\\MySeleniumProject\\MyEcommerceJavaMavenProject\\image\\abc.png"));
+
+		// Compare images
+		ImageDiffer imgDiff = new ImageDiffer();
+		ImageDiff diff = imgDiff.makeDiff(expectedImage, actualImage);
+
+		if (diff.hasDiff()) {
+			System.out.println("❌ Images are different!");
+		} else {
+			System.out.println("✅ Images match!");
+		}
 
 	}
 
--- MyEcommerceJavaMavenProject/src/test/java/RegistrationPage/TC_30.java
+++ MyEcommerceJavaMavenProject/src/test/java/RegistrationPage/TC_30.java
@@ -1,24 +1,13 @@
 package RegistrationPage;
 
 import org.openqa.selenium.By;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import Base.Base;
 import GlobalUtility.CommonUtils;
 
-public class TC_28 extends Base {
-
-	@AfterMethod
-	public void downApplication() throws InterruptedException {
-		if (driver != null) {
-			Thread.sleep(4000);
-			driver.quit();
-		}
-
-	}
+public class TC_30 extends Base {
 
 	@BeforeMethod
 	public void setupt() {
@@ -27,7 +16,7 @@ public class TC_28 extends Base {
 
 	@Test
 
-	void verifyRegisterPageDiffEnv() throws InterruptedException {
+	void verifyOneMoreTestCasesAtTime() throws InterruptedException {
 
 		driver.get("https://tutorialsninja.com/demo/");
 		driver.findElement(By.linkText("My Account")).click();
@@ -41,10 +30,14 @@ public class TC_28 extends Base {
 		driver.findElement(By.xpath("//label[normalize-space()='Yes']//input[@name='newsletter']")).click();
 		driver.findElement(By.xpath("//input[@name='agree']")).click();
 		driver.findElement(By.xpath("//input[@value='Continue']")).click();
-		String expectedmsg = "Your Account Has Been Created!";
-		String actmsg = driver.findElement(By.xpath("//h1[normalize-space()='Your Account Has Been Created!']"))
-				.getText();
-		Assert.assertEquals(actmsg, expectedmsg);
+	}
+
+	@AfterMethod
+	public void downApplication() throws InterruptedException {
+		if (driver != null) {
+			Thread.sleep(4000);
+			driver.quit();
+		}
 
 	}
 
